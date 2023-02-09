2fd14df485f3bce7be03933ef04b4fb49f123fee
theeggymaster <111299393+theeggymaster@users.noreply.github.com>
2023-02-09 09:23:57 -0800

    more UI

--- Assets/Scenes/PaintBrush.cs
+++ Assets/Scenes/PaintBrush.cs
@@ -15,10 +15,9 @@ public class PaintBrush : MonoBehaviour {
     [SerializeField] private Texture2D canvasTexture;
     //The canvas will be a plane with a texture
 
-    // Start is called before the first frame updatex
+    // Start is called before the first frame update
     void Start() {
         //Get the texture of the canvas
-        //
     }
     //when the tip of the paint brush touches the canvas
     private void OnTriggerEnter(Collider other) {
@@ -27,9 +26,14 @@ public class PaintBrush : MonoBehaviour {
             return;
         }
         //Get the position of the paint brush tip
-        Vector2 pixelUV = paintBrushTip.position;
-        //Get the pixel coordinates of the paint brush tip
-        Vector2 pixelPos = new Vector2(pixelUV.x * canvasTexture.width, pixelUV.y * canvasTexture.height);
+        Vector3 pixelUV = paintBrushTip.position;
+        //get the position of the canvas
+        Vector3 canvasPos = canvas.transform.position;
+        //get the position of the tip of the paint brush relative to the canvas
+        Vector2 canvasOrigin = other.bounds.min;
+        canvasOrigin.y = other.bounds.max.y;
+        Vector2 pixelPos = new Vector2(pixelUV.x - canvasOrigin.x, canvasOrigin.y - canvasPos.y);
+        //pixelPos.x -= other.bounds.
         //Paint the canvas
         PaintPosition(pixelPos);
     }
@@ -39,6 +43,9 @@ public class PaintBrush : MonoBehaviour {
         Color[] pixels = canvasTexture.GetPixels();
         //Get the pixel coordinates of the paint brush tip
         Vector2Int pixelPosInt = new Vector2Int(Mathf.RoundToInt(pixelPos.x), Mathf.RoundToInt(pixelPos.y));
+        //scale the pixel coordinates to the canvas texture
+        pixelPosInt.x = Mathf.RoundToInt(pixelPosInt.x * canvasTexture.width);
+        pixelPosInt.y = Mathf.RoundToInt(pixelPosInt.y * canvasTexture.height);
         //Get the pixel coordinates of the paint brush tip
         int pixelIndex = pixelPosInt.y * canvasTexture.width + pixelPosInt.x;
         //Paint the canvas
